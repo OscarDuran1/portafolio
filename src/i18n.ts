cda6ae7b23132fd5865d30eab477c117b8d64209
Oscar Durán <oscar.dura1@gmail.com>
2025-08-19 16:24:46 -0600

    Mejora en el uso de los archivos de traducción

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,25 +1,23 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from 'i18next-http-backend';
 
-import translationEN from "./locales/en/translation.json";
-import translationES from "./locales/es/translation.json";
-
-const resources = {
-  en: {
-    translation: translationEN,
-  },
-  es: {
-    translation: translationES,
-  },
-};
-
-i18n.use(LanguageDetector).use(initReactI18next).init({
-  resources,
-  fallbackLng: "es",
-  interpolation: {
-    escapeValue: false, // react already safes from xss
-  },
+i18n
+  .use(HttpApi)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "es",
+    interpolation: {
+      escapeValue: false, // react already safes from xss
+    },
+    backend: {
+      loadPath: '/portafolio/locales/{{lng}}/{{ns}}.json',
+    },
+    react: {
+      useSuspense: true,
+    },
 });
 
 export default i18n;
